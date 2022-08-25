f248a0023f6011d56a5405edf93a43bf67fe409f
Tenthsin <55300869+OnTheFifth@users.noreply.github.com>
2022-08-25 14:54:47 -0700

    potential fix to show cash
    revert from action defined to use event.
    
    css styles changed to ensure everything in its own little place

--- cfx-server-data/resources/[Main-Power]/MP-Elements/html/scripts.js
+++ cfx-server-data/resources/[Main-Power]/MP-Elements/html/scripts.js
@@ -1,60 +1,57 @@
-var hidden = false;
+var seen = false;
 
 $(document).ready(function () {
     window.addEventListener('message', function (event) {
         var data = event.data;
-        var action = data.action;
-        if (action == 'toggle') {
-            if (hidden) {
+        if (event.data.action == "toggle") {
+            if (seen) {
                 $('.container').fadeOut();
             } else {
                 $('.container').fadeIn();
             }
-            hidden = !hidden;
+            seen = !seen;
         }
 
-        if (action == 'show') {
+        if (event.data.action == "show") {
             $('.container').fadeIn();
-            hidden = true;
+            seen = true;
         }
 
-        if (action == 'hide') {
+        if (event.data.action == "hide") {
             $('.container').fadeOut();
-            hidden = false;
+            seen = false;
         }
 
-        if (action == 'setValue') {
+        if (event.data.action == "setValue") {
             setValue(data.key, data.value)
         }
 
-        if (action == 'addcash') {
-            var element = $("<div class= 'cashchange'><font style='color: rgb(255,255,255); font-weight: 700; margin-right: 6px;'>+ <span id='dollar-plus'>$</span>" + data.value + "</font></div>");
+        if (event.data.action == "addcash") {
+            var element = $("<div class='cashchange'><font style='color: rgb(255, 255, 255); font-weight: 700;hasrfa margin-right: 6px;'>+ <span id='dollar-plus'>$  </span>" + data.value + "</font></div>");
             $("#cashchange").append(element);
-            setTimeout(function (){
-                $(element).fadeOut(1000, function () {$(this).remove();});
+            setTimeout(function () {
+                $(element).fadeOut(1000, function () { $(this).remove(); });
             }, 1000);
-        } 
-
-        if (action == 'removeCash') {
-            var element = $("<div class= 'cashchange'><font style='color: rgb(255,255,255); font-weight: 700; margin-right: 6px;'>- <span id='dollar-min'>$</span>" + data.value + "</font></div>");
-            $("#cashchange").append(element);
-            setTimeout(function (){
-                $(element).fadeOut(1000, function () {$(this).remove();});
-            }, 1000);
-        } 
-
-
-        if (action == 'updateValue') {
-            updateValue(data.key, data.value)
         }
 
+        if (event.data.action == "removecash") {
+            var element = $("<div class='cashchange'><font style='color: rgb(255, 255, 255); font-weight: 700;hasrfa margin-right: 6px;'>- <span id='dollar-min'>$  </span>" + data.value + "</font></div>");
+            $("#cashchange").append(element);
+            setTimeout(function () {
+                $(element).fadeOut(1000, function () { $(this).remove(); });
+            }, 1000);
+        }
+
+        if (event.data.action == "updateValue") {
+            updateValue(data.key, data.value)
+        }
     })
 })
 
 function setValue(key, value) {
-    $('#' + key).html("<span id='dollar'>$</span>" + value)
+    $('#' + key).html("<span id='dollar'>$  </span>" + value)
 }
 
 function updateValue(key, value) {
-    $('#' + key).html("<span id='dollar'>$</span>" + value)
+    $('#' + key).html("<span id='dollar'>$  </span>" + value)
 }
